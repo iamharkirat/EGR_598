9c0915d7ddd0715c0221e1f9f88791fad99d8127
iamharkirat <hchahal1@asu.edu>
2023-07-29 14:23:54 -0500

    Wrote suitable comments on the code

--- Code/data_collect.py
+++ Code/data_collect.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -8,42 +9,64 @@ import os
 import csv
 import time
 
+# Define the node for data collection
 class DataCollectionNode(Node):
 
     def __init__(self):
+        # Initialize the node
         super().__init__('data_collection_node')
+
+        # Create subscriptions to image and velocity topics
         self.image_subscriber = self.create_subscription(Image, '/color/preview/image', self.image_callback, 10)
         self.cmd_vel_subscriber = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
+
+        # Initialize velocity message
         self.current_cmd_vel = Twist()
-        self.data_dir = 'data'  # Specify the directory to store the data
+
+        # Define the data directory and csv file path
+        self.data_dir = 'data'  
         self.csv_file = os.path.join(self.data_dir, 'data.csv')
 
+        # Create the data directory if it doesn't exist
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
 
+        # Create a CSV file and write the header
         with open(self.csv_file, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(['timestamp', 'image_path', 'linear_x', 'angular_z'])
 
+    # Define the callback function for velocity messages
     def cmd_vel_callback(self, cmd_vel_msg):
+        # Save the current velocity message
         self.current_cmd_vel = cmd_vel_msg
 
+    # Define the callback function for image messages
     def image_callback(self, img_msg):
+        # Convert the image message to a numpy array and save the image
         img = np.array(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
         timestamp = time.time()
         image_path = os.path.join(self.data_dir, f"{timestamp:.6f}.png")
         cv2.imwrite(image_path, img)
 
+        # Open the CSV file and append the current data
         with open(self.csv_file, 'a', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow([timestamp, image_path, self.current_cmd_vel.linear.x, self.current_cmd_vel.angular.z])
 
+# Define the main function
 def main(args=None):
+    # Initialize the ROS client library
     rclpy.init(args=args)
+
+    # Create and spin the data collection node
     data_collection_node = DataCollectionNode()
     rclpy.spin(data_collection_node)
+
+    # Destroy the node and shutdown rclpy
     data_collection_node.destroy_node()
     rclpy.shutdown()
 
+# Run the main function if the script is run directly
 if __name__ == '__main__':
     main()
--- Code/vel_subscriber.py
+++ Code/vel_subscriber.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -8,20 +9,32 @@ import numpy as np
 from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
 
+# Define the LaneFollower node
 class LaneFollower(Node):
 
     def __init__(self):
         super().__init__('lane_follower')
         self.bridge = CvBridge()
+
+        # Create subscriptions to the image topic and create a publisher to the velocity command topic
         self.image_subscriber = self.create_subscription(Image,'/color/preview/image',self.image_callback,10)
         self.cmd_vel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+
+        # Load the trained keras model
         self.model = keras.models.load_model('/home/ani/final_ws/my_model.h5')
+
+        # Initialize the label encoder with the angular_z values used during training
         self.label_encoder = LabelEncoder()
-        self.label_encoder.fit([-0.079766, 0.00000, 0.079766])  # Replace these values with the actual angular_z values used during training
+        self.label_encoder.fit([-0.079766, 0.00000, 0.079766])
 
     def image_callback(self, msg):
+        # Convert the image message to cv2 image
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        # Preprocess the image
         preprocessed_image = self.preprocess_image(cv_image)
+
+        # Use the model to predict the angular_z probability
         angular_z_prob = self.model.predict(preprocessed_image)
 
         # Convert the predictions to label indices
@@ -33,29 +46,46 @@ class LaneFollower(Node):
         # Get the first (and only) value in the array
         angular_z = angular_z.item(0)
 
+        # Send velocity command
         self.send_velocity_command(angular_z)
 
     def preprocess_image(self, image):
+        # Resize the image to the size expected by the model
         new_width = 224
         new_height = 224
         preprocessed_image = cv2.resize(image, (new_width, new_height))
+        
+        # Normalize the image pixel values
         preprocessed_image = preprocessed_image.astype('float32') / 255.0
+
+        # Expand the dimensions of the image to include the batch size
         preprocessed_image = np.expand_dims(preprocessed_image, axis=0)
         return preprocessed_image
 
     def send_velocity_command(self, angular_z):
+        # Initialize a Twist message
         twist = Twist()
+
+        # Set the linear and angular velocities
         twist.linear.x = 0.03  # Adjust the linear velocity according to your preference
         twist.angular.z = angular_z
+
+        # Publish the velocity command
         self.cmd_vel_publisher.publish(twist)
         print("Published angular_z:", angular_z)
 
+# Define the main function
 def main(args=None):
     rclpy.init(args=args)
+
+    # Initialize and spin the lane_follower node
     lane_follower = LaneFollower()
     rclpy.spin(lane_follower)
+
+    # Destroy the node and shutdown rclpy
     lane_follower.destroy_node()
     rclpy.shutdown()
 
+# Run the main function if the script is run directly
 if __name__ == '__main__':
     main()
